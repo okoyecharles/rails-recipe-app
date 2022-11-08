59e48d2b7d1fdaf08b8c79fdc2a01c81a34c2957
Bria222 <techconsultants.bree@gmail.com>
2022-11-08 13:05:35 +0300

    configure routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'users#index'
   resources :foods, only: %i[index show new create]
+  resources :recipes, only: %i[index show new create destroy]
 end
