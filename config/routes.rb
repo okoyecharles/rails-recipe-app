fa6ce2e5ebc4f7956e92a517c169caa425de7cd0
OkoyeCharles <okoyecharles509@gmail.com>
2022-11-08 09:51:28 +0100

    Setup github actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
-  root to: "users#index"
-
-
+  root to: 'users#index'
+  resources :foods, only: %i[index show new create]
 end
