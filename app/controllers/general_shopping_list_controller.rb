5d5582aa2308bcf029f46b949ed770a2fd8a8c95
Bria222 <techconsultants.bree@gmail.com>
2022-11-10 16:44:32 +0300

    Add general shopping list functionality

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,4 +1,5 @@
 class GeneralShoppingListController < ApplicationController
    def index
+     @general_list = current_user.general_list
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,26 @@ class User < ApplicationRecord
   has_many :foods
   has_many :recipes
   validates :name, presence: true
+
+  def general_list
+    # get total quantity of foods required
+    foods_quantity_sum = RecipeFood.select(:food_id, 'SUM(quantity) as quantity_sum').group(:food_id).order('quantity_sum')
+    list = []
+  
+    # select foods for current_user with insufficient quantity
+    foods.each do |food|
+      expected_quantity = foods_quantity_sum.where(food:).first.quantity_sum || nil
+  
+      next unless expected_quantity
+      next unless food.quantity < expected_quantity
+  
+      list << {
+        food: food,
+        remaining_quantity: expected_quantity - food.quantity,
+        price: (expected_quantity - food.quantity) * food.price
+      }
+    end
+    list
+  end
+  
 end
